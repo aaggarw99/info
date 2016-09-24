bb46052cff499f14399093773233fb0afec6a7b9
aaggarw99 <aaggarw99@gmail.com>
2016-09-23 20:08:08 -0500

    implementing jquery w/ loading styles

--- aggarwal_assets/aggarwal_javascript/script.js
+++ aggarwal_assets/aggarwal_javascript/script.js
@@ -1,8 +1,6 @@
-if(strpos($_SERVER['HTTP_REFERER'], $_SERVER['HTTP_HOST']) == false){
   $(document).ready(function() {
       setTimeout(function(){
           $('body').addClass('loaded');
       }, 2000);
 
   });
-}
