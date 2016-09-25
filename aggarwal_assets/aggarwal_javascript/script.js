d82ead135c1cd66ddafb3f260218f9579d3b5f8b
aaggarw99 <aaggarw99@gmail.com>
2016-09-24 21:14:59 -0500

    working on slide in effect nav

--- aggarwal_assets/aggarwal_javascript/script.js
+++ aggarwal_assets/aggarwal_javascript/script.js
@@ -1,6 +1,12 @@
-  $(document).ready(function() {
-      setTimeout(function(){
-          $('body').addClass('loaded');
-      }, 2000);
-
+$(document).ready(function() {
+    setTimeout(function(){
+        $('body').addClass('loaded');
+    }, 2000);
   });
+
+$(document).ready(function(){
+  setTimeout(function() {
+    $("nav").slideDown(400);
+  }, 2000)
+    
+});
