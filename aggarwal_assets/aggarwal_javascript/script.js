432dcd4ef08e96bd05989f9010f0c327520452fc
aaggarw99 <aaggarw99@gmail.com>
2016-09-24 22:29:01 -0500

    added sticky transition ins

--- aggarwal_assets/aggarwal_javascript/script.js
+++ aggarwal_assets/aggarwal_javascript/script.js
@@ -12,4 +12,28 @@ $(document).ready(function(){
   setTimeout(function() {
     $('.indexNav nav').slideDown(400).fadeIn(400);
   }, 2300);
+
+  setTimeout(function() {
+    $('#mathSticky').fadeIn(300);
+  }, 2100);
+
+  setTimeout(function() {
+    $('#athleticsSticky').fadeIn(400);
+  }, 2200);
+
+  setTimeout(function() {
+    $('#communitySticky').fadeIn(350);
+  }, 2300);
+
+  setTimeout(function() {
+    $('#scienceSticky').fadeIn(250);
+  }, 2400);
+
+  setTimeout(function() {
+    $('#vismedSticky').fadeIn(450);
+  }, 2500);
+
+  setTimeout(function() {
+    $('#appSticky').fadeIn(500);
+  }, 2600);
 });
