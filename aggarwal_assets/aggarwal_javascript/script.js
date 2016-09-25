b8ed7d7d132353522b35b1c134cbf06d9b9c70ae
aaggarw99 <aaggarw99@gmail.com>
2016-09-24 21:23:14 -0500

    new loading spinner

--- aggarwal_assets/aggarwal_javascript/script.js
+++ aggarwal_assets/aggarwal_javascript/script.js
@@ -4,9 +4,13 @@ $(document).ready(function() {
     }, 2000);
   });
 
+
+// eventually add if statement whether coming from external link
+// then have the slide in effect happen with a shorter delay.
+
 $(document).ready(function(){
   setTimeout(function() {
     $("nav").slideDown(400).fadeIn(400);
-  }, 2300)
+  }, 2300);
 
 });
