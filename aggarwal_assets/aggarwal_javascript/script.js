ed20a206276b11115970f4f8b276158a7718a1d1
aaggarw99 <aaggarw99@gmail.com>
2016-09-28 11:17:43 -0500

    slideshow

--- aggarwal_assets/aggarwal_javascript/script.js
+++ aggarwal_assets/aggarwal_javascript/script.js
@@ -5,6 +5,18 @@ $(document).ready(function() {
   });
 
 
+$("#slideshow > div:gt(0)").hide();
+
+  setInterval(function() {
+    $('#slideshow > div:first')
+      .fadeOut(1000)
+      .next()
+      .fadeIn(1000)
+      .end()
+      .appendTo('#slideshow');
+},  3000);
+
+
 // eventually add if statement whether coming from external link
 // then have the slide in effect happen with a shorter delay.
 
