a3d80dff9fbdf31684c59765911dc514e2145ddb
aaggarw99 <aaggarw99@gmail.com>
2017-01-13 11:29:20 -0600

    js

--- aggarwal_assets/aggarwal_javascript/script.js
+++ aggarwal_assets/aggarwal_javascript/script.js
@@ -15,20 +15,28 @@ $(document).ready(function(){
   }, 2300);
 });
 
-var modal = document.getElementById('modal_link');
-var button = document.getElementById("button");
-var close = document.getElementsByClassName("close")[0];
+// Get the modal
+var modal = document.getElementById('myModal');
 
-button.onclick  = function() {
-  modal.style.display = "block";
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
 }
 
-close.onclick = function(){
-  modal.style.display = "none";
-}
-
-window.onclick = function(event){
-  if(event.target == modal) {
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
     modal.style.display = "none";
-  }
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
