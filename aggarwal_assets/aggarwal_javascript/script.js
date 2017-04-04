ea4308896ca817edbfe9a26b8b4e63fce956bd13
aaggarw99 <aaggarw99@gmail.com>
2017-04-04 11:22:08 -0500

    modified mailing options on every page

--- aggarwal_assets/aggarwal_javascript/script.js
+++ aggarwal_assets/aggarwal_javascript/script.js
@@ -1,14 +1,25 @@
 var popup_status = false;
 
 $(document).ready(function() {
-    setTimeout(function() {
-        $('body').addClass('loaded');
-    }, 2000);
-  });
+  if(document.referrer.split('/')[2]!=location.hostname){
+      setTimeout(function() {
+            $('body').addClass('loaded');
+        }, 2000);
+    //User came from other domain or from direct
+  }else{
+    //User came from another page on your site
+    $('body').addClass('loaded');
+  }
+});
 $(document).ready(function() {
-  setTimeout(function() {
+  if(document.referrer.split('/')[2]!=location.hostname){
+      setTimeout(function() {
+        $('.indexNav nav').slideDown(400).fadeIn(400);
+      }, 2300);
+  }else{
     $('.indexNav nav').slideDown(400).fadeIn(400);
-  }, 2300);
+  }
+
 
 });
 
@@ -51,44 +62,22 @@ $(document).on('mouseup', function (e)
 
 function copy_to_clipboard() {
   var textArea = document.createElement("textarea");
-
-//
-// *** This styling is an extra step which is likely not required. ***
-//
-// Why is it here? To ensure:
-// 1. the element is able to have focus and selection.
-// 2. if element was to flash render it has minimal visual impact.
-// 3. less flakyness with selection and copying which **might** occur if
-//    the textarea element is not visible.
-//
-// The likelihood is the element won't even render, not even a flash,
-// so some of these are just precautions. However in IE the element
-// is visible whilst the popup box asking the user for permission for
-// the web page to copy to the clipboard.
-//
-
 // Place in top-left corner of screen regardless of scroll position.
 textArea.style.position = 'fixed';
 textArea.style.top = 0;
 textArea.style.left = 0;
-
 // Ensure it has a small width and height. Setting to 1px / 1em
 // doesn't work as this gives a negative w/h on some browsers.
 textArea.style.width = '2em';
 textArea.style.height = '2em';
-
 // We don't need padding, reducing the size if it does flash render.
 textArea.style.padding = 0;
-
 // Clean up any borders.
 textArea.style.border = 'none';
 textArea.style.outline = 'none';
 textArea.style.boxShadow = 'none';
-
 // Avoid flash of white box if rendered for any reason.
 textArea.style.background = 'transparent';
-
-
 textArea.value = "aaggarw99@gmail.com";
 
 document.body.appendChild(textArea);
